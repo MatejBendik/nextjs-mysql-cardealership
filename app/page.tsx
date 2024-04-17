a066914a8a1dff0c22bb87ba4c97fc6f5923ebc5
MatejBendik <matejbendik.mb@gmail.com>
2024-04-17 23:13:45 +0200

    Add api with get requests and fetch example

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,49 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+
+type Car = {
+  id: number;
+  brand: string;
+  model: string;
+  year: number;
+};
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/cars")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return <div>No data</div>;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    <>
+      <div className="flex items-center justify-center">
+        <div className="flex justify-center items-center h-24 bg-gray-800 w-1/2 rounded-md">
+          <div className="text-white text-xl">Cars dealership</div>
+        </div>
       </div>
-    </main>
+      <div className="px-20 py-4">
+        {data.map((car: Car) => (
+          <div
+            key={car.id}
+            className="text-center py-4 my-2 bg-gray-700 rounded"
+          >
+            <div className="text-white text-xl">
+              {car.brand} {car.model} ({car.year})
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
